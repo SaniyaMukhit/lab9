10240eb396130efe0bfb74043da800ed054c1c34
SaniyaMukhit <you@example.com>
2022-10-25 21:24:16 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,29 @@
+// Problem #2
+function perimeter_Rectangle(a,b) {
+    let perimeter = (a+b)*2;
+    console.log(perimeter)
+}
+perimeter_Rectangle(3,2)
+
+// Problem #12
+function check_numbers(x) {
+    if (x >= 0 && x <= 1)
+    {
+        console.log ("given number is between 0 and 1") ;
+      } 
+      else 
+      {
+        console.log ("given number is NOT between 0 and 1") ; 
+      }
+}
+check_numbers(3)
+
+// Problem #3
+function Area_Circle(radius) {
+    let area = pi * radius * radius;
+    console.log(area)
+}
+pi = 3.14
+Area_Circle(4)
+
+
